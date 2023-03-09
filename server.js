1b06630443c3f549aff6e9cdc3342f7db257d883
foxwayne1 <fox.wayne1@gmail.com>
2023-03-09 12:47:13 +0000

    update

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+const PORT = process.env.PORT || 3500
 
-app.listen(3000, () => console.log(`listing at http://localhost:3000`))
+app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', require('./routes/root'))
+
+app.listen(PORT, () => console.log(`listing at http://localhost:${PORT}`))
